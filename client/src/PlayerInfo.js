f9cf6a6b2168fb96ba8c8ab16ee58603070fb963
ryankayne <coope1rk@gmail.com>
2019-12-06 13:41:25 -0500

    final styling and code check

--- client/src/PlayerInfo.js
+++ client/src/PlayerInfo.js
@@ -1,14 +1,31 @@
 import React, {useEffect} from "react";
+import styled from 'styled-components';
+
+const Container = styled.section`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+`;
+
+const PlayerCard = styled.div`
+    height: 200px;
+    width: 400px;
+    background-color: darkgrey;
+    border-radius: 30px;
+    margin: 2%;
+`;
 
 export default function PlayerInfo(props){
     useEffect(() => {
        console.log(props)
       }, [props]);
     return (
-        <div>
+        <Container>
+        <PlayerCard>
             <h1>Name: {props.name}</h1>
             <h2>Country: {props.country}</h2>
             <h2>Searches: {props.searches}</h2>
-        </div>
+        </PlayerCard>
+        </Container>
     )
 }

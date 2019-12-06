0353b0fab97efe9fd6f1bd4f8c73af8819ca9e9d
ryankayne <coope1rk@gmail.com>
2019-12-06 14:00:33 -0500

    MVP

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as rtl from '@testing-library/react';
 import { render } from '@testing-library/react';
+
+afterEach(rtl.cleanup);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('testing testing testing', () => {
+  const divtest = rtl.render(
+    <div className="testing">TESTING...</div>
+  );
+  const element = divtest.queryByText(/testing/i);
+})
 
 test('contains player', () => {
   const { findByText } = render(<App />);
